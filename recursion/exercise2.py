72d1f650947518b0f545fcd7cac3a7f3fe17618c
Ephraim Malinga <ephraim.malinga@gmail.com>
2022-10-19 15:49:17 +0300

    sum of first n integers

---
+++ recursion/exercise2.py
@@ -0,0 +1,14 @@
+"""
+Write a recursive Python function that returns the sum of the first n integers. 
+(Hint: The function will be similiar to the factorial function!)
+"""
+
+def sumn(n):
+    if n == 0:
+        return 0
+    else:
+       return n + sumn(n-1)
+
+
+for i in range(1,5):
+    print(sumn(i))
